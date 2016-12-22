d81016ace3766afa3572780e7324e9e910a2c649
Andy Sloane <andy@a1k0n.net>
2016-12-21 16:18:34 -0800

    animate a triangle
    ok, this thing is smooth as butter, wow. we're inches away from a really
    great looking animated 3d object

--- arduboy3d.ino
+++ arduboy3d.ino
@@ -8,23 +8,22 @@ void setup() {
   // put your setup code here, to run once:
   arduboy_.begin();
   screen_ = arduboy_.getBuffer();
+}
 
-  for (int i = 0; i < 8; i++) {
-    FillVLine(i, 7, 0xff, screen_+i);
-  }
-  for (int i = 0; i < 8; i++) {
-    FillVLine(0, i, 0x55 << (i & 1), screen_+i + 8);
-  }
+uint16_t frame = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
 
-  uint8_t pat[] = {0xff, 0xff, 0xff, 0xff};
+  float y1 = 16*32 + 16*30.0 * sin(0.01*frame);
+  
+  uint8_t pat[4];
+  GetDitherPattern((int) (9 + 8*sin(0.002*frame)), pat);
   FillTriangle(
       16-1, 16*48,
-      48*16 + 8, 16*33 - 1,
+      48*16 + 8, (int16_t) y1,
       127*16, 16*64,
       pat, screen_);
   arduboy_.display(true);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  ++frame;
 }
